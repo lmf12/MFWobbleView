25b29945edb557d06d601df6f193a898686a0e9c
Lyman <limianfeng12@gmail.com>
2019-04-18 20:34:29 +0800

    初始化项目

---
+++ MFWobbleView/MFWobbleView.m
@@ -0,0 +1,35 @@
+//
+//  MFWobbleView.m
+//  MFWobbleViewDemo
+//
+//  Created by Lyman Li on 2019/4/18.
+//  Copyright © 2019年 Lyman Li. All rights reserved.
+//
+
+#import "MFWobbleView.h"
+
+@implementation MFWobbleView
+
+- (instancetype)initWithFrame:(CGRect)frame {
+    self = [super initWithFrame:frame];
+    if (self) {
+        [self commonInit];
+    }
+    return self;
+}
+
+- (instancetype)initWithCoder:(NSCoder *)aDecoder {
+    self = [super initWithCoder:aDecoder];
+    if (self) {
+        [self commonInit];
+    }
+    return self;
+}
+
+#pragma mark - Private
+
+- (void)commonInit {
+    self.backgroundColor = [UIColor blueColor];
+}
+
+@end
---
+++ MFWobbleViewDemo/ViewController.m
@@ -0,0 +1,35 @@
+//
+//  ViewController.m
+//  MFWobbleViewDemo
+//
+//  Created by Lyman Li on 2019/4/18.
+//  Copyright © 2019年 Lyman Li. All rights reserved.
+//
+
+#import "MFWobbleView.h"
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@property (nonatomic, strong) MFWobbleView *wobbleView;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    [self setupUI];
+}
+
+- (void)setupUI {
+    UIImage *image = [UIImage imageNamed:@"sample.jpg"];
+    CGFloat width = self.view.frame.size.width;
+    CGFloat height = self.view.frame.size.width / image.size.width * image.size.height;
+    self.wobbleView = [[MFWobbleView alloc] initWithFrame:CGRectMake(0, 100, width, height)];
+    [self.view addSubview:self.wobbleView];
+}
+
+@end
